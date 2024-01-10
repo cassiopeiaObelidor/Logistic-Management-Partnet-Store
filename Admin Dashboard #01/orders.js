5698c6c4c60dc591c5647d2553842da3e64cc7d0
CASSIIOPEIA OBELIDOR <143474600+cassiopeiaObelidor@users.noreply.github.com>
2024-01-10 13:14:18 +0800

    first commit

---
+++ Admin Dashboard #01/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Jersey',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'School Uniform',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'T-shirt',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
